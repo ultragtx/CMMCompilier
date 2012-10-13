5a66310529ba8473cac75923aeaa7839325c057c
ultragtx <ultragtx@gmail.com>
2012-10-13 15:36:33 +0800

    Initial Commit

---
+++ CMMCompiler/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CMMCompiler
+//
+//  Created by Xinrong Guo on 12-10-13.
+//  Copyright (c) 2012年 Xinrong Guo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
